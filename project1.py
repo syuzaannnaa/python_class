ca9e8bad5d803058d6ba992e5b6d0ebeea5a8380
syuzaannnaa <khachatryansyuzanna48@gmail.com>
2025-08-16 22:04:12 +0400

    Create project1.py

---
+++ project1.py
@@ -0,0 +1,28 @@
+if number == 0:
+    print(f"The number {number} is zero.")
+if number > 0
+    print(f"The number {number} is positive.")
+if number % 2 ! = 0:
+    print(f"The number {number} is odd.")
+
+
+
+if age >= 18
+   print("You are an adult.")
+   print("You are eligible to vote.")
+elif age >= 0 and age < 18
+    print("You are not adult.")
+    print("You are not eliglible to vote.")
+else:
+    print("Age cannot be a negative number. Please enter a valid age.")
+
+
+
+for number in range(1,11):
+    print(number)
+
+
+sum = 0
+for number in range(1,101):
+    sum += number
+print(f"The sum of numbers from 1 to 100 is: {sum}")
